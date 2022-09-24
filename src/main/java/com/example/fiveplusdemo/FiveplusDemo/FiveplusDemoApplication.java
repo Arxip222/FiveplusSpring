e01cd26fcc13befb029c3aec70f397b930755e69
Arxip222 <Arhipbirukov@gmail.com>
2022-09-24 23:47:02 +0300

    Initial commit

---
+++ src/main/java/com/example/fiveplusdemo/FiveplusDemo/FiveplusDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.fiveplusdemo.FiveplusDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiveplusDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiveplusDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fiveplusdemo/FiveplusDemo/FiveplusDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fiveplusdemo.FiveplusDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiveplusDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
