aa1b805c705003f98be9c27b70342d653330b1c2
Arxip222 <Arhipbirukov@gmail.com>
2022-10-02 12:37:16 +0300

    without vaadin

---
+++ src/main/java/com/example/fiveplusdemo/FiveplusDemo/controller/ParentsController.java
@@ -0,0 +1,48 @@
+package com.example.fiveplusdemo.FiveplusDemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/parents_home")
+public class ParentsController {
+
+    @GetMapping("/profile")
+    public String parentsProfile() {
+        return "parents_home";
+    }
+
+    @GetMapping("/")
+    public String parentsMain() {
+        return "redirect:profile";
+    }
+
+    @GetMapping("/chats")
+    public String parentsChats() {
+        return "chats";
+    }
+
+    @GetMapping("/history")
+    public String parentsLessonsHistory() {
+        return "history";
+    }
+
+    @GetMapping("/activation")
+    public String parentsActivation() {
+        return "activation";
+    }
+
+    @GetMapping("/help")
+    public String parentsHelp() {
+        return "help";
+    }
+
+    @GetMapping("/404")
+    public String parentsError404() {
+        return "error404";
+    }
+
+
+}
--- src/main/resources/static/assets/js/main.js
+++ src/main/resources/static/assets/js/main.js
@@ -18,7 +18,6 @@
       return document.querySelector(el)
     }
   }
-
   /**
    * Easy event listener function
    */
